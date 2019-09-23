4723e99a07fd0d982f7d442bb6dbce1cfa56bd80
Jay Thakkar <thakkarj@ymail.com>
2019-09-23 12:24:32 +1200

    SearchFEature Added along with Date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import addFiles from "./components/addfiles.component";
+import Navbar from './components/navbar.component';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import searchFiles from "./components/search.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+    
+      <div className="container">
+      <br/>
+      <Route path="/addfiles" exact component={addFiles} />
+      <Route path="/" exact component={searchFiles} />
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/addfiles.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class addFiles extends Component {
+    render() {
+        return(
+            <div>
+                <p>You are on Add Files Section!</p>
+            </div>
+        )
+    }
+}
