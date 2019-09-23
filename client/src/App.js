65f800d8344374b97d1f3eeaadd29f4d08eb0f3a
Jay Thakkar <thakkarj@ymail.com>
2019-09-23 12:40:10 +1200

    Search

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Todo from './components/Todo';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      hello world!
+    </div>
+  );
+}
+
+export default App;
