f8b898e7a4241ce8e0d5dac3cf357d69788c799e
Jay Thakkar <thakkarj@ymail.com>
2019-10-05 16:11:57 +1300

    enhanced resuts page and added savesearch buttons

--- client/src/components/results.component.js
+++ client/src/components/results.component.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
 import { render } from "react-dom";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import axios from 'axios';
+import React, { Component, useState } from 'react';
+import "react-datepicker/dist/react-datepicker.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../App.css";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 
 export default class Results extends Component {
 
@@ -10,11 +16,53 @@ export default class Results extends Component {
 
     super(props);
 
-
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.ontoggle = this.toggle.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  
     this.state = {
-      articles: []
+      articles: [],
+      description: this.props.description,
+      results: [],
+      toggle: true,
+      showSearch: false
     }
 
+}
+
+toggle = () => {
+  console.log('print me' + this.state.showSearch)
+  const { showSearch } = this.state;
+  this.setState({
+    showSearch: !showSearch
+  })
+}
+
+
+onChangeDescription(e) {
+  this.setState({
+    description: e.target.value
+  })
+}
+
+onChangeEnd(date) {
+  this.setState({
+    end: date
+  })
+}
+
+
+onSubmit(e) {
+  e.preventDefault();
+
+  const Search = {
+    description: this.state.description
+
+
+  }
+
+  console.log(Search);
+  window.location = '/results';
 }
 
     componentDidMount() {
@@ -32,7 +80,96 @@ export default class Results extends Component {
     const { articles } = this.state;
     return (
       <div>
-          <h1>Search Results</h1>
+         <h1>Search Results</h1>
+         
+        <div>
+        <form onSubmit={this.onSubmit}>
+          
+          <input type="text" required className="form-control"
+            value={this.state.description}
+            onChange={this.onChangeDescription} padding-bottom="20px" />            
+            <a href="/savedsearches">Saved Searches</a>
+
+          <div class="d-flex justify-content-end">
+          <button className="btn btn-dark" onClick={this.save}>Save Search</button>
+          </div>
+        </form>
+        <div>
+                <form onSubmit={this.onSubmit}>
+                    <div className="i-am-centered" >
+                        <div className="column-container" >
+                            <label className="help" > Start Date: </label>
+                            <DatePicker
+                                selected={this.state.start}
+                                onChange={this.onChangeStart}
+                                peekNextMonth
+                                showMonthDropdown
+                                showYearDropdown
+                                maxDate={new Date()}
+                                dropdownMode="select"
+                                placeholderText="MM/DD/YYYY"
+                                required
+                            />
+                            <label className="help" > End Date: </label>
+                            <DatePicker
+                                selected={this.state.end}
+                                onChange={this.onChangeEnd}
+                                peekNextMonth
+                                showMonthDropdown
+                                showYearDropdown
+                                maxDate={new Date()}
+                                dropdownMode="select"
+                                placeholderText="MM/DD/YYYY"
+                                required
+                            />
+                        </div >
+
+
+                    </div>
+                    <br></br>
+
+                    <div className="btn-group btn-block">
+                        <label className="drop">If:</label>
+
+                        <select className="form-control" id="exampleSelect1" value={this.state.method}
+                            onChange={this.onChangeMethod} required>
+                            <option value="">Select Method</option>
+                            <option>TDD</option>
+                            <option>Scrum</option>
+                            <option>BDD</option>
+                            <option>Kanban</option>
+                            <option>Agile</option>
+                            <option>Walerfall</option>
+                            <option>lean</option>
+
+                        </select>
+
+                        <label className="drop">Operator: </label>
+                        <select className="form-control" id="exampleSelect2" value={this.state.operator}
+                            onChange={this.onChangeOperator} required>
+                            <option value="">Select Operator</option>
+                            <option>Contains</option>
+                            <option>Doesn't Contain</option>
+                            <option>Begins with</option>
+                            <option>Ends With</option>
+                            <option>Is Equal to</option>
+                            <option>Is less than</option>
+                            <option>Is more than</option>
+
+                        </select>
+
+                        <label className="drop">Value: </label>
+                        <input type="text" required className="form-control"
+                            value={this.props.value}
+                            onChange={this.onChangeValue} padding-bottom="20px" />
+                    </div>
+                    {this.state.show}
+                    <div class="i-am-centered-button" padding-top="20px">
+                        <input type="submit" value="Search" className="btn btn-dark" padding-top="20px" />
+                    </div>
+                </form>
+            </div>
+      </div>
           <br></br>
         <ReactTable
           data={articles}
@@ -91,7 +228,7 @@ export default class Results extends Component {
               asc: true
             }
           ]}
-          defaultPageSize={20}
+          defaultPageSize={10}
           className="-striped -highlight"
         />
         <br />
