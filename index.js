92bbf8656fa1d206848485d37edd0742eb43cc31
Jay Thakkar <thakkarj@ymail.com>
2019-10-09 20:37:00 +1300

    'Deployment'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // require('dotenv').config();
 
@@ -36,6 +37,15 @@ app.use('/articles', articlesRouter);
 
 const searchesRouter = require('./routes/searches');
 app.use('/searches', searchesRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( 'client/build' ));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
